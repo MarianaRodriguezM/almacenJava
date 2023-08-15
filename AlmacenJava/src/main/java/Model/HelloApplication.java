8f2b02baa0d27d5a2d148819c06fe81194f32a05
marianapro <marianapantera7@gmail.com>
2023-08-15 16:47:14 -0500

    se ingresa Interfaces PersonasNaturalJuridica y PersonasNatural Juridica comprarProductos y comprarProductos

---
+++ AlmacenJava/src/main/java/Model/HelloApplication.java
@@ -0,0 +1,20 @@
+package Model;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Almacen.fxml"));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
