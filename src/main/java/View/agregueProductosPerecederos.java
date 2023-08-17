830b9ecf91865da3733da14ec3062d1fafe6cb0a
Mariana Muñoz Rodriguez <127903568+MarianaRodriguezM@users.noreply.github.com>
2023-08-17 16:06:43 -0500

    Subo interfaces tipos de Productos

---
+++ src/main/java/View/agregueProductosPerecederos.java
@@ -0,0 +1,14 @@
+package View;
+
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class agregueProductosPerecederos {
+    public Button EliminarProductoPerecedero;
+
+    public void EventoAgregarProductoPerecedero(MouseEvent actionEvent) {
+    }
+
+    public void EventoEliminarProductoPerecedero(MouseEvent mouseEvent) {
+    }
+}
